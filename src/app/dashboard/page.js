5b6bad4074d07d22f2469f8a68233ff42be419dd
Aadi Umrani <aadi.p.um@gmail.com>
2024-07-30 19:51:31 -0400

    Can now view data

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,34 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TextField, Button, Container, Typography, Box, IconButton } from '@mui/material'
 import { CircularProgress } from "@mui/material";
 import { AddRounded, SearchRounded } from '@mui/icons-material';
 
 export default function Dashboard() {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const [pantry, setPantry] = useState([])
+
+  const fetchData = async () => {
+    const res = await fetch('/api/pantry')
+    const data = await res.json()
+    setPantry(data)
+  }
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const res = await fetch('/api/auth/check')
+      const data = await res.json()
+      if (!data.success) {
+        router.push('/')
+        return
+      }
+      fetchData();
+    };
+    checkAuth();
+  }, [])
   return (
     <div>
       <Container>
@@ -23,6 +45,27 @@ export default function Dashboard() {
             sx={{ width: '80%' }}
           />
           <SearchRounded color="primary" sx={{ marginY: 2, marginX: 2 }} />
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}>
+          {pantry.map((item, index) => (
+            <Box key={index} sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: 2,
+              marginY: 1,
+              border: '1px solid #ccc',
+              borderRadius: 4,
+            }}>
+              <Typography>{item.name}</Typography>
+              <Typography>{item.quantity}</Typography>
+            </Box>
+          ))}
         </Box>
       </Container>
     </div>
