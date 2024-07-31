faa9695cae7f01f2115b45545798a9d1df16baa2
Aadi Umrani <aadi.p.um@gmail.com>
2024-07-31 09:58:13 -0400

    Can now add new items

---
+++ src/app/components/addItemModal.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import { Modal, Box, Typography, TextField, Button } from '@mui/material';
+import firebase from '@/app/firebase'
+
+const AddItemModal = ({ open, handleClose }) => {
+    const [imgUrl, setImgUrl] = useState('');
+
+    const handleUpload = async (e) => {
+        const file = e.target.files[0];
+        const storageRef = firebase.storage().ref();
+        const fileRef = storageRef.child(file.name);
+        await fileRef.put(file);
+        setImgUrl(await fileRef.getDownloadURL());
+    };
+
+    const addItem = async (item) => {
+        try {
+            const user = firebase.auth().currentUser;
+            if (!user) {
+                return { error: 'User not authenticated.' };
+            }
+            const collectionRef = firebase.firestore().collection("pantry");
+            await collectionRef.add(item);
+            const snapshot = await collectionRef.get();
+            const documents = snapshot.docs.map(doc => doc.data());
+            console.log(documents);
+
+            return { success: true, message: 'Item added successfully.' };
+        } catch (error) {
+            console.error(error);
+            return { error: 'Something went wrong.' };
+        }
+    }
+    const handleAddItem = async () => {
+        const itemName = document.getElementById('item-name').value;
+        const itemBrand = document.getElementById('item-brand').value;
+        const itemPrice = document.getElementById('item-price').value;
+        const itemQty = document.getElementById('item-qty').value;
+        const img = imgUrl;
+        if (!itemName) {
+            console.error('Item name is required.');
+            alert('Item name is required.');
+            return;
+        } else if (!itemBrand) {
+            console.error('Item brand is required.');
+            alert('Item brand is required.');
+            return;
+        } else if (!itemPrice) {
+            console.error('Item price is required.');
+            alert('Item price is required.');
+            return;
+        } else if (!itemQty) {
+            console.error('Item quantity is required.');
+            alert('Item quantity is required.');
+            return;
+        } else if (!img) {
+            console.error('Item image is required.');
+            alert('An image is required.');
+            return;
+        }
+        const item = {
+            name: itemName,
+            brand: itemBrand,
+            price: Number(itemPrice),
+            qty: Number(itemQty),
+            img: img
+        };
+        const result = await addItem(item);
+        if (result.success) {
+            handleClose();
+        } else {
+            console.error(result.error);
+        }
+    };
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+        >
+            <Box sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: 400,
+                bgcolor: 'white',
+                boxShadow: 24,
+                borderRadius: 2,
+                p: 4,
+            }}>
+                <Typography id="modal-title" variant="h6" component="h2">
+                    Add New Item
+                </Typography>
+                <TextField
+                    id="item-name"
+                    label="Name"
+                    variant="outlined"
+                    fullWidth
+                    sx={{ mt: 2 }}
+                />
+                <TextField
+                    id="item-brand"
+                    label="Brand"
+                    variant="outlined"
+                    fullWidth
+                    sx={{ mt: 2 }}
+                />
+                <TextField
+                    id="item-price"
+                    label="Price"
+                    variant="outlined"
+                    fullWidth
+                    sx={{ mt: 2 }}
+                />
+                <TextField
+                    id="item-qty"
+                    label="Quantity"
+                    variant="outlined"
+                    fullWidth
+                    sx={{ mt: 2 }}
+                />
+                <label htmlFor="upload-image" style={{ display: 'inline-block' }}>
+                    <input
+                        id="upload-image"
+                        type="file"
+                        onChange={handleUpload}
+                        style={{ display: 'none' }}
+                    />
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        component="span"
+                        sx={{ mt: 2 }}
+                    >
+                        Upload Image
+                    </Button>
+                </label>
+                <Button variant="contained" color="primary" sx={{ mt: 2, mx: 2 }} onClick={handleAddItem}>
+                    Add Item
+                </Button>
+            </Box>
+        </Modal>
+    );
+};
+
+export default AddItemModal;
--- src/app/page.js
+++ src/app/page.js
@@ -2,33 +2,39 @@
 
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import { TextField, Button, Container, Typography, Box, IconButton } from '@mui/material'
+import { TextField, Button, Container, Typography, Box, IconButton, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material'
 import { AddRounded, SearchRounded } from '@mui/icons-material';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import firebase from '@/app/firebase'
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import Header from './components/header';
+import AddItemModal from './components/addItemModal';
 
 export default function Inventory() {
   const router = useRouter()
   const [loading, setLoading] = useState(false)
   const [pantry, setPantry] = useState([])
+  const [openAddItemModal, setOpenAddItemModal] = useState(false)
+
+  const handleClose = () => {
+    setOpenAddItemModal(false)
+  }
 
   async function fetchData(req) {
     try {
-        const user = firebase.auth().currentUser;
-        if (!user) {
-          return { error: 'User not authenticated.' };
-        }
-        const collectionRef = firebase.firestore().collection("pantry");
-        const snapshot = await collectionRef.get();
-        const documents = snapshot.docs.map(doc => doc.data());
-        console.log(documents);
-        setPantry(documents);
+      const user = firebase.auth().currentUser;
+      if (!user) {
+        return { error: 'User not authenticated.' };
+      }
+      const collectionRef = firebase.firestore().collection("pantry");
+      const snapshot = await collectionRef.get();
+      const documents = snapshot.docs.map(doc => doc.data());
+      console.log(documents);
+      setPantry(documents);
     } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: 'Something went wrong.' }, { status: 500 });
+      console.error(error);
+      return NextResponse.json({ error: 'Something went wrong.' }, { status: 500 });
     }
   }
 
@@ -69,37 +75,72 @@ export default function Inventory() {
           flexDirection: 'row',
           justifyContent: 'center',
         }}>
-          <AddRounded color="primary" sx={{ marginY: 2, marginX: 2 }} />
+          <IconButton sx={{ marginX: 2 }} onClick={
+            () => setOpenAddItemModal(true)
+          }>
+            <AddRounded color="primary" />
+          </IconButton>
           <TextField
             label="Search"
             variant='outlined'
             sx={{ width: '80%' }}
           />
-          <SearchRounded color="primary" sx={{ marginY: 2, marginX: 2 }} />
+          <IconButton sx={{ marginX: 2 }} onClick={
+            () => setOpenAddItemModal(true)
+          }>
+            <SearchRounded color="primary" />
+          </IconButton>
         </Box>
         <Box sx={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
         }}>
-          {pantry.map((item, index) => (
-            <Box key={index} sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              padding: 2,
-              marginY: 1,
-              border: '1px solid #ccc',
-              borderRadius: 4,
-            }}>
-              <Typography>{item.name}</Typography>
-              <Typography>{item.price}</Typography>
-              <Typography>{item.qty}</Typography>
-            </Box>
-          ))}
+          <Grid container spacing={2}>
+            {pantry.map((item, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Box sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  padding: 2,
+                  marginY: 1,
+                  border: '1px solid #ccc',
+                  borderRadius: 4,
+                }}>
+                  <img src={item.img} alt="placeholder" style={{ height: '200px', borderRadius: '4px 4px 0 0' }} />                  
+                  <TableContainer component={Paper} sx={{
+                    boxShadow: 0,
+                  }}>
+                    <Table>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>Name</TableCell>
+                          <TableCell>{item.name}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Brand</TableCell>
+                          <TableCell>{item.brand}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Price</TableCell>
+                          <TableCell>{item.price}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Quantity</TableCell>
+                          <TableCell>{item.qty}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </Container>
+      <AddItemModal open={openAddItemModal} handleClose={handleClose} />
     </div>
   )
 }
