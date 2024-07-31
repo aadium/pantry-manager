b48ea2675edcbdf203c4c3b3b0a022b6b879b1c2
Aadi Umrani <aadi.p.um@gmail.com>
2024-07-31 01:10:06 -0400

    Added logout button

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -3,17 +3,35 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import firebase from '@/app/firebase'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const Header = () => {
+    async function logout() {
+        try {
+            await firebase.auth().signOut();
+            console.log('User signed out successfully.');
+            return { success: true };
+        } catch (error) {
+            console.error('Error during logout:', error);
+            return { error: 'Something went wrong.' };
+        }
+    }
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Pantry
                 </Typography>
-                <Button color="inherit">Logout</Button>
+                <Button color="inherit" onClick={
+                    async () => {
+                        const response = await logout();
+                        if (response.success) {
+                            window.location.href = '/login';
+                        }
+                    }
+                }>Logout</Button>
             </Toolbar>
         </AppBar>
     );
